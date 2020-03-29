81e7740e7a2966b3824f7be242c6952e0dab7584
Guilherme Dantas <guidanoli@hotmail.com>
2020-03-28 23:42:53 -0300

    Barebones

--- README.md
+++
@@ -1,2 +0,0 @@
-# pydslib
-Python Data Structure Library
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+exec(open("src/version.py").read())
+
+setup(
+    name='pydslib',
+    version=__version__,
+    author='Guilherme Dantas',
+    author_email='guidanoli@hotmail.com',
+    
+	description='A collection of well known data structures'
+				'implemented in Python.',
+	long_description=open('README.rst').read(),
+	url='https://github.com/guidanoli/pydslib',
+    license='MIT',
+	
+    packages=["pydslib"],
+	
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: Implementation :: CPython',
+	]
+)
+
